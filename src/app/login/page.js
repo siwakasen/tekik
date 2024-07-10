038516e46381cf267f4df32fdcca26d4d2f5d091
siwakasen <siwakasen@gmail.com>
2024-07-10 11:24:20 +0700

    update login view

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { Input } from "@nextui-org/input";
 import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/card";
 import { Divider } from "@nextui-org/react"
@@ -17,7 +17,10 @@ export default function Page() {
     const validateEmail = (value) => value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i);
     const toggleVisibility = () => setIsVisible(!isVisible);
     const router = useRouter();
-
+    const inputRef = useRef(null);
+    useEffect(() => {
+        inputRef.current.focus();
+    }, []);
 
     const handleLogin = async (e) => {
         e.preventDefault();
@@ -58,17 +61,20 @@ export default function Page() {
     }
     return (
         <main>
-            <div className="flex justify-center h-screen items-center bg-gradient-to-tl from-sky-500 via-white  to-teal-300">
-                <Card className="w-full mx-4 max-w-[400px] max-h-[800px]">
+            <div className="hidden md:h-screen md:flex justify-center   items-center bg-gradient-to-tl  from-sky-300 via-green-100  to-green-400">
+                <Card className="w-full mx-4 max-w-[400px] max-h-[800px]  md:block">
                     <CardHeader className="flex gap-3 p-5">
                         <div className="flex flex-col w-full">
-                            <p className="text-3xl font-bold text-center text-gray-700">Selamat Datang !</p>
+                            <p className="text-2xl font-bold text-center text-gray-700">Selamat Datang </p>
+                            <p className="text-2xl font-bold text-center text-gray-700" >di <span className="text-green-800">Padukuhan Tekik 🌾</span></p>
                         </div>
                     </CardHeader>
                     <Divider />
                     <form onSubmit={handleLogin}>
                         <CardBody className="flex gap-4 py-8 px-6">
+
                             <Input
+                                key={"outside"}
                                 type="email"
                                 label="Email"
                                 value={email}
@@ -76,6 +82,9 @@ export default function Page() {
                                 color={validEmail && email ? "" : email ? "danger" : ""}
                                 errorMessage="Email tidak valid"
                                 onChange={handleChangeEmail}
+                                className="my-6"
+                                ref={inputRef}
+                                placeholder="example@email.com"
                             />
                             <Input
                                 endContent={
@@ -105,6 +114,58 @@ export default function Page() {
                         </CardFooter>
                     </form>
                 </Card>
+            </div>
+            <div className="md:hidden h-screen ">
+
+                <div className="p-10 bg-gradient-to-tr h-full  ">
+                    <div className="pt-8">
+                        <p className="text-xl sm:text-2xl font-bold  text-gray-900">Selamat datang di</p>
+                        <p className="text-2xl sm:text-3xl font-bold  text-green-800">Padukuhan Tekik 🌾</p>
+                        <p className="mt-4 text-sm sm:text-md text-gray-600">
+                            Silahkan masukkan email dan password untuk mengakases halaman admin.
+                        </p>
+                    </div>
+                    <form>
+                        <Input
+                            key={"outside"}
+                            type="email"
+                            label="Email"
+                            value={email}
+                            isInvalid={!validEmail}
+                            color={validEmail && email ? "" : email ? "danger" : ""}
+                            errorMessage="Email tidak valid"
+                            onChange={handleChangeEmail}
+                            className="my-6"
+                            ref={inputRef}
+                        />
+                        <Input
+                            endContent={
+                                <button className="focus:outline-none" type="button" onClick={toggleVisibility}>
+                                    {isVisible ? (
+                                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                    ) : (
+                                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                    )}
+                                </button>
+                            }
+                            type={isVisible ? "text" : "password"}
+                            label="Password"
+                            value={password}
+                            isInvalid={!validPassword}
+                            color={validPassword && password ? "" : password ? "danger" : ""}
+                            errorMessage="Password tidak boleh kosong"
+                            onChange={handleChangePassword}
+                            className="mt-6 mb-4"
+
+                        />
+                        <div className="flex flex-col items-start pt-4">
+                            <button className="w-20 bg-blue-500 p-1 mb-2 rounded text-white" size="lg" type="submit" >
+                                <span className="text-lg font-semibold">Login</span>
+                            </button>
+                            <p className="text-sm font-thin mb-4 text-gray-500">Akses ke halaman admin</p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </main >
     )
